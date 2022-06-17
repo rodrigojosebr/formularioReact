4e9a798eb60dc501f9c13782da4cce1d78165ce6
Rodrigo José <rodrigo.angelis@grupoeureka.com.br>
2022-06-16 21:20:00 -0300

    Criação Header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+export const Header = () => {
+  return (
+    <S.Container>
+      <h1>Cadastro de Desenvolvedor</h1>
+      <p>Faça o seu cadastro em nossa lista dos desesperados</p>
+    </S.Container>
+  );
+};
